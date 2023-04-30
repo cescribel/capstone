0efd468bec659df1c6564cf6f4f31da7f2f99985
cescribel <84375916+cescribel@users.noreply.github.com>
2023-05-01 01:49:18 +0800

    update login

---
+++ login/script.js
@@ -0,0 +1,6 @@
+const showLoginBtn = document.getElementById('show-login');
+const loginForm = document.getElementById('login-form');
+
+showLoginBtn.addEventListener('click', () => {
+  loginForm.style.display = 'block';
+});
